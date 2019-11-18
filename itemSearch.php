6040f40923aa48c91706ba12023e52940197fe22
Kemazata <Cory_Light@hotmail.com>
2019-11-18 18:42:28 -0500

    Create itemSearch.php

---
+++ itemSearch.php
@@ -0,0 +1,10 @@
+<?php include "header.php"; ?><h2>Item Search</h2>
+
+    <form method="post">
+      <label for="itemName">Item Name</label>
+      <input type="text" name="itemName" id="itemName">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <a href="index.php">Back to home</a>
+<?php include "footer.php";?>
