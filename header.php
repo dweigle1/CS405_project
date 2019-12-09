69dbfc7e787216e87e5e772dfd5c61167a2308a9
Cory-Light <48219367+Cory-Light@users.noreply.github.com>
2019-12-09 10:35:40 -0500

    Update header.php

--- header.php
+++ header.php
@@ -63,7 +63,7 @@ else
     </ul>
 	
     <form action="gatherItems.php" method="post" class="form-inline">
-		<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" id="Search">
+		<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="Search">
 		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 	</form>
 	<?php 
