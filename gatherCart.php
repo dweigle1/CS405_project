8d5a068fd84e116cfe2c76b3878dde38a25c3c10
Cory-Light <48219367+Cory-Light@users.noreply.github.com>
2019-12-10 14:36:16 -0500

    Update gatherCart.php

--- gatherCart.php
+++ gatherCart.php
@@ -4,6 +4,7 @@
 
 
 if(isset($_SESSION["login_user"])){
+    echo $_SESSION["login_user"];
 	$conn = new mysqli($host, $username, $password, $dbname);
         if ($conn->connect_error) {
                die("Connection failed: " . $conn->connect_error);
