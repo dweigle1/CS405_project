b5c22802ae3a9016cabec0f56c0617c95214dab2
dweigle1 <45437595+dweigle1@users.noreply.github.com>
2019-12-09 19:20:23 -0500

    will this work?

--- Stats.php
+++ Stats.php
@@ -60,12 +60,13 @@ $conn = new mysqli($host, $username, $password, $dbname);
 		echo "<th>timeOrdered</th>";
 		echo "<th>ProdName</th>";
 		echo "<th>Quantity</th>";
-
+		$dept = array();
 		echo "</tr>";
 		while ($row = mysqli_fetch_assoc($result)) {
     	echo "<tr>";
     	foreach ($row as $field => $value) { 
         echo "<td>" . $value . "</td>";
+			$dept[]= ($field == 'timeOrdered') ? $value : NULL;
 		}
     	echo "</tr>";
 		}
@@ -76,7 +77,37 @@ $conn = new mysqli($host, $username, $password, $dbname);
 
 
 
-
+	
 ?>
-
+<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Simple Column Chart with Index Labels"
+	},
+	data: [{
+		type: "column", //change type to bar, line, area, pie, etc
+		//indexLabel: "{y}", //Shows y value on all Data Points
+		indexLabelFontColor: "#5A5757",
+		indexLabelPlacement: "outside",   
+		dept: <?php echo json_encode($dept, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>                              
 
