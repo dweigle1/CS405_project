969018f5e79d286f4c5dd00738f527fee5de8c96
Christian Tooley <christian.tooley@hotmail.com>
2019-12-11 00:33:17 -0500

    yes

--- gatherCart.php
+++ gatherCart.php
@@ -20,25 +20,28 @@ if(isset($_SESSION["login_user"])){
             die("Connection failed: " . $conn->connect_error);
 	} else{
 		
+		echo '<form method="post" class="form-inline">
+			<input class="form-control mr-sm-2" type="text" placeholder="Enter the Address" aria-label="placeOrder" name="placeOrder">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Submit Order</button>
+		</form>';
+		
 		$login = $_SESSION["login_user"];
 		$sql = "SELECT Products.ProdName, Products.Price, Promotions.Discount as SalePrice FROM Products JOIN ShopsFor ON Products.PID = ShopsFor.PID AND ShopsFor.UserName = '".$login."' Left JOIN Promotions ON Promotions.PID = Products.PID Order BY ProdName ASC";	
 		$result = mysqli_query($conn, $sql);
         $conn->close();
 
         echo "<br>";
-        echo "<table border='1' style='width:100%'>";
-        echo "<tr>";
+        echo "<table border='1' style='width:80%; margin: 0 auto;' class='table'>";
+        echo "<tr><thead class='thead-dark'>";
         
         echo "<th>Product Name</th>";
         echo "<th>Price</th>";
-        echo "<th>Discounted Price</th>";
 
-        echo "</tr>";
+        echo "</tr></thead>";
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
-            foreach ($row as $field => $value) { 
-                echo "<td>" . $value . "</td>";
-            }
+            echo "<td>". $row["ProdName"] ."</td>";
+			echo "<td>". ($row["SalePrice"] == NULL ? $row["Price"] : $row["SalePrice"]) ."</td>";
             echo "</tr>";
         }
         echo "</table>";		
@@ -47,32 +50,45 @@ if(isset($_SESSION["login_user"])){
 
 ?>
 
-<form method="post" class="form-inline">
-    <input class="form-control mr-sm-2" type="text" placeholder="Enter the Address" aria-label="placeOrder" name="placeOrder">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Submit Order</button>
-</form>
-
 <?php
 
-if(isset($_POST["placeOrder"])){
+if(isset($_POST["placeOrder"]))
+{
+	##Create SQL Connection
     $conn = new mysqli($host, $username, $password, $dbname);
+	
+	##Get new OrderID
     $sql = "SELECT MAX(OrderID) FROM Orders;";	
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    $orderID = $row["MAX(OrderID)"] + 1;
-    $today = date("y/m/d");
+	$orderID = $row["MAX(OrderID)"] + 1;
+	mysqli_free_result($result);
+	
+	##Insert Order info into Orders
+    $today = date("Y-m-d H:i:s");
     $sql = "INSERT INTO Orders (OrderID, status, timeOrdered, UserName, Address) VALUES ('".$orderID."', 'Pending', '".$today."', '".$login."', '".$_POST["placeOrder"]."')";
-    mysqli_free_result($result); 
-    if (mysqli_query($conn, $sql)) {
-        $sql = "DELETE FROM ShopsFor WHERE UserName = '".$login."'";
-        $result = mysqli_query($conn, $sql);
-        echo "Successfully inserted product information!";
-    } else {
-        echo "Error: " . $sql . "" . mysqli_error($conn);
-    }
-    header("Refresh:0");
-    $conn->close();
+	mysqli_query($conn, $sql);
+	
+	##Insert cart into OrderProducts
+	$sql = "select PID, count(*) AS Cnt from ShopsFor WHERE UserName = '" . $login . "' group by PID;";
+	$result = mysqli_query($conn, $sql);
 
+	while ($row = mysqli_fetch_row($result)) 
+	{
+		echo "!!";
+		$sql = "INSERT INTO OrderProducts (OrderID, Quantity, PID) VALUES ('" . $orderID ."', '" . $row[1] . "', '" . $row[0] . "');";
+		$result = mysqli_query($conn, $sql);
+	}
+	mysqli_free_result($result);
+	
+    ##Delete cart
+	$sql = "DELETE FROM ShopsFor WHERE UserName = '".$login."'";
+	mysqli_query($conn, $sql);
+	
+	echo "Successfully inserted product information!";
+	
+    $conn->close();
+	##header("Refresh:0");
 }
 ?>
 
--- index.php
+++ index.php
@@ -23,17 +23,14 @@ echo "<thead class='thead-dark'><tr>";
 
 echo "<th>Product Name</th>";
 echo "<th>Price</th>";
-echo "<th>Discounted Price</th>";
 echo "<th> Add to Cart </th>";
-echo "<th></th>";
 
 echo "</tr></thead>";
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>";
     echo "<td>". $row["ProdName"] ."</td>";
-            echo "<td>". $row["Price"] ."</td>";
-            echo "<td>". $row["Discount"] ."</td>";
-            echo "<td><form method='post'><input type='submit' value='Add To Cart' name='addToCart - " .$row["PID"]. "'></input></form></td>";
+	echo "<td>". ($row["Discount"] == NULL ? $row["Price"] : $row["Discount"]) ."</td>";
+	echo "<td><form method='post'><input type='submit' value='Add To Cart' name='addToCart - " .$row["PID"]. "'></input></form></td>";
     echo "</tr>";
 }
 echo "</table>";
