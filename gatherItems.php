e3789d6944483432a76f79b5c7a0560931d934e1
Kemazata <Cory_Light@hotmail.com>
2019-12-10 19:34:22 -0500

    Update gatherItems.php

--- gatherItems.php
+++ gatherItems.php
@@ -40,7 +40,7 @@ if(isset($_POST["Search"])){
             foreach ($row as $field => $value) { 
                 echo "<td>" . $value . "</td>";
             }
-            echo "<td><form name='addToCart' method='post'><input type='submit' value='$row[0]' name='addToCart'></input></form></td>";
+            echo "<td><form name='addToCart' method='post'><input type='submit' value='addToCart' name='addToCart'></input></form></td>";
             echo "</tr>";
         }
         echo "</table>";		
