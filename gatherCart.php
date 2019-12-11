622d737e4fb90d9e27c070d30663dfe92bab2bb8
Kemazata <Cory_Light@hotmail.com>
2019-12-10 22:37:20 -0500

    Update gatherCart.php

--- gatherCart.php
+++ gatherCart.php
@@ -58,11 +58,9 @@ if(isset($_POST["placeOrder"])){
     $conn = new mysqli($host, $username, $password, $dbname);
     $sql = "SELECT MAX(OrderID) FROM Orders;";	
     $result = mysqli_query($conn, $sql);
-    $conn->close();
     $row = mysqli_fetch_assoc($result);
     $orderID = $row["MAX(OrderID)"] + 1;
     $today = date("y/m/d");
-    $conn = new mysqli($host, $username, $password, $dbname);
     $sql = "INSERT INTO Orders (OrderID, status, timeOrdered, UserName, Address) VALUES ('".$orderID."', 'Pending', '".$today."', '".$login."', '".$_POST["placeOrder"]."')";
     mysqli_free_result($result); 
     if (mysqli_query($conn, $sql)) {
@@ -72,7 +70,36 @@ if(isset($_POST["placeOrder"])){
     } else {
         echo "Error: " . $sql . "" . mysqli_error($conn);
     }
+    $conn = new mysqli($host, $username, $password, $dbname);
+    if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+	} else{
+		
+		$login = $_SESSION["login_user"];
+		$sql = "SELECT DISTINCT Products.ProdName, Products.Price, Promotions.Discount as SalePrice FROM Products JOIN ShopsFor ON Products.PID = ShopsFor.PID AND ShopsFor.UserName = '".$login."' Left JOIN Promotions ON Promotions.PID = Products.PID Order BY ProdName ASC";	
+		$result = mysqli_query($conn, $sql);
+        
+
+        echo "<br>";
+        echo "<table border='1' style='width:100%'>";
+        echo "<tr>";
+        
+        echo "<th>Product Name</th>";
+        echo "<th>Price</th>";
+        echo "<th>Discounted Price</th>";
+
+        echo "</tr>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            foreach ($row as $field => $value) { 
+                echo "<td>" . $value . "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";		
+    }
     $conn->close();
+
 }
 ?>
 
