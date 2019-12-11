bf0c088b602cb3dad2a2ef148cf2af84c835ce9e
Kemazata <Cory_Light@hotmail.com>
2019-12-10 22:38:25 -0500

    Update gatherCart.php

--- gatherCart.php
+++ gatherCart.php
@@ -70,34 +70,7 @@ if(isset($_POST["placeOrder"])){
     } else {
         echo "Error: " . $sql . "" . mysqli_error($conn);
     }
-    $conn = new mysqli($host, $username, $password, $dbname);
-    if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-	} else{
-		
-		$login = $_SESSION["login_user"];
-		$sql = "SELECT DISTINCT Products.ProdName, Products.Price, Promotions.Discount as SalePrice FROM Products JOIN ShopsFor ON Products.PID = ShopsFor.PID AND ShopsFor.UserName = '".$login."' Left JOIN Promotions ON Promotions.PID = Products.PID Order BY ProdName ASC";	
-		$result = mysqli_query($conn, $sql);
-        
-
-        echo "<br>";
-        echo "<table border='1' style='width:100%'>";
-        echo "<tr>";
-        
-        echo "<th>Product Name</th>";
-        echo "<th>Price</th>";
-        echo "<th>Discounted Price</th>";
-
-        echo "</tr>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            foreach ($row as $field => $value) { 
-                echo "<td>" . $value . "</td>";
-            }
-            echo "</tr>";
-        }
-        echo "</table>";		
-    }
+    header("Refresh:0");
     $conn->close();
 
 }
